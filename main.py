6561534ba4ce93219ff2f72e81b6604f31b1e7c3
aayush <ayyush00sharma@gmail.com>
2024-09-17 10:53:36 +0530

    UC1: Added Employee Attendance Check

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+import random
+
 print("Welcome to Employee Wage Computation Program")
+
+# UC1: Check Employee Attendance
+attendance = random.choice([0, 1])
+if attendance == 1:
+    print("Employee is Present")
+else:
+    print("Employee is Absent")
