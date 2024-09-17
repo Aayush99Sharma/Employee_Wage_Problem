15f37a87d7a5cd8ff6532638b25b13de4184e3f9
aayush <ayyush00sharma@gmail.com>
2024-09-17 10:59:17 +0530

    UC2: Added Daily Employee Wage Calculation

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ if attendance == 1:
     print("Employee is Present")
 else:
     print("Employee is Absent")
+
+#UC2: Daily Employee Wage
+wage_per_hour = 20
+full_day_hour = 8
+daily_wage = wage_per_hour * full_day_hour
+print(f"Daily Employee Wage: {daily_wage}")
+
+
